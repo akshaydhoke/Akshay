0fa2e804c24cda0a5d4918c342b29f5a6ecf5465
Akshay Dhoke <72206708+akshaydhoke@users.noreply.github.com>
2021-09-13 11:00:01 +0530

    second change 2
    add another sop

--- Akshay/src/com/Aks/Akhay1.java
+++ Akshay/src/com/Aks/Akhay1.java
@@ -3,9 +3,7 @@ package com.Aks;
 public class Akhay1 {
    public static void main(String[] args) {
 	   
-	System.out.println("Akshay");
-	   
-	System.out.println("Dhoke");
+	System.out.println("Akshay Dhoke....");
 	
 }
 }
